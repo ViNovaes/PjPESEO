bd7d2d036ab7ddf10f02d54f84660ebf8bc34024
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2024-03-05 11:52:26 -0300

    Header,Cadastro,Login Prontos, Pagina home em andamento

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 
 //bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header/Header';
-// import Login from './components/Login/Login';
+
+//import Header from './components/Header/Header';
+import Cadastro from './components/Cadastro/Cadastro';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login'
 
 function App() {
   return (
     <div className="App">
-         <Header/>
-         {/* <Login/> */}
+         <Home/>
+         <Login/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,44 @@
-//bootstrsp
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react"
+import logo from '../../img/logo.png'
+import { Row, Col, Container, Nav, Navbar, Button } from "react-bootstrap";
+import style from './header.module.css'
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-//img
 
-
-export default () => {
+const Header = () => {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-            <Navbar.Brand href="#home">
-            <img
-              src="'../Header/logo.png'"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="logo"
-            />
-          </Navbar.Brand>'
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Trilhas</Nav.Link>
-                        <Nav.Link href="#link">Quem Somos</Nav.Link>
-                        <Nav.Link href="#link">Entrar</Nav.Link>
-                        <Nav.Link href="#link">Criar Conta</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+        <Navbar  fixed="top" expand="lg" className={style.contNav}>
+            <Container >
+
+                <Row>
+                    <Col>
+                        <Navbar.Brand href="#home">
+                            <img src={logo} alt="Logo" fluit />
+                        </Navbar.Brand> 
+                    </Col>
+                    </Row>
+
+                    <Row>
+                    <Col>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="#Trilhas"  className={style.custom_nav_link}>Trilhas</Nav.Link>
+                                <Nav.Link href="#Quem somos" className={style.custom_nav_link}>Quem somos</Nav.Link>
+                                <Nav.Link  className={style.custom_nav_link}>|</Nav.Link>
+                                <Nav.Link href="#Entrar" className={style.custom_nav_link}>Entrar</Nav.Link>
+                            </Nav>
+                            <Button variant="primary" className={ style.btnAccouny }> 
+                            <FontAwesomeIcon icon={faUser}></FontAwesomeIcon> Criar Conta
+                            </Button>
+                        </Navbar.Collapse>
+                    </Col>
+                </Row>
+
             </Container>
         </Navbar>
+    );
+}
 
-
-    )
-};
+export default Header;
